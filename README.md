17b8657b226dd59f3bd87e855df12d9424b1455a
BSombi <benjsombi@gmail.com>
2023-02-14 15:27:32 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Build a Web App to Group & Plot Excel Files in Python with Streamlit
+
+Did you know that you can quickly develop web applications in just a few lines of code using Python? Streamlit is a Python module that makes it very easy to quickly develop web apps. In this particular web app, we will be transforming & visualizing Excel files. The user can visualise Univariate and Bivariate Data.
+
+## Video Tutorial
+
+[![YouTube Video](https://img.youtube.com/vi/ZDffoP6gjxc/0.jpg)](https://youtu.be/ZDffoP6gjxc)
+
+## Author
+
+- Benjamin from Gr8mindshub
+- YouTube: https://youtube.com/c/CodingIsFun
+- Website: https://gr8mindshub.com
---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st  # pip install streamlit
+import pandas as pd  # pip install pandas
+import seaborn as sns  #pip install seaborn
+from matplotlib import pyplot as plt  #pip install matplotlib
+
+
+# Setting Page Titles
+st.set_page_config(page_title='Excel Data Visualisation')
+st.title('Employee Attrition Data Visualisation 📈')
+st.subheader('Feed me with your Excel file')
+
+
+#Uploading data
+uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
+if uploaded_file:
+    st.markdown('---')
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+
+    #Selecting Specific Categorical Variables
+    groupby_column = st.selectbox(
+        'Which Categorical Variable would you like to analyse?',
+        ('BusinessTravel', 'Department', 'Education', 'EducationField','EnvironmentSatisfaction', 
+        'Gender', 'JobRole', 'JobSatisfaction', 'MaritalStatus', 'OverTime', 'PerformanceRating', 
+        'Attrition'),
+    )
+
+    # Here we plot the simple graph 
+    st.subheader(f'Graph showing the distribution of {groupby_column}')
+    fig = sns.catplot(df, x=groupby_column, kind="count", palette="ch:start=.2,rot=-.3") #shrink = 0.8)
+    st.pyplot(fig)
+
+
+    # Here we plot the relationship between Attrition and the selected categorical variables
+    st.subheader(f'Relationship between Attrition and {groupby_column}')
+    fig1 = sns.displot(df,x=groupby_column, hue="Attrition", multiple="dodge", shrink=0.8)
+    st.pyplot(fig1)
+
+
+    # Here we create a selector for continuous variables
+    groupby_cont_column = st.selectbox(
+        'Which Continuous Variables would you like to analyse?',
+        ('Age', 'CostOfHiring', 'MonthlySalary', 'NumCompaniesWorked', 'TotalWorkingYears',
+        'TrainingTimesLastYear', 'YearsAtCompany', 'YearsSinceLastPromotion'),
+    )
+    
+
+    # Here we plot the relationship between Attrition and the selected continuous variables
+    st.subheader(f'Relationship between Attrition and {groupby_cont_column}')
+    fig3 = sns.catplot(df, kind="box", y=groupby_cont_column, x="Attrition") #hue="Attrition")
+    st.pyplot(fig3)
